199b7beacefee72eb026a91c1bef81bafddd2fce
dverheijke <darin.verheijke@student.uantwerpen.be>
2020-12-22 09:40:10 +0100

    Added functionality for only LSH, not using a forest.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3.9
 
 from jaccard import ShingleSetGenerator, jaccard
-from minhash import get_minforest, query
+from minhash import get_minforest, query, minhash_lsh, query_lsh
 from collections.abc import Generator, Iterable
 from csv import reader
 from re import split
@@ -48,13 +48,18 @@ if __name__ == "__main__":
     csv_reader = read_csv(filename)
     shingle_gen = ShingleSetGenerator(read_data(csv_reader), 2)
     shingle_sets = list(shingle_gen)
-    forest = get_minforest(database['article'], 256)
-    q = "Richard Krajicek ensured an all-Dutch final in the ATP's 600,000 dollar event here Sunday when he beat Omar Camporese of Italy 6-3, 7-5. Authorities arrested a close aide to Indonesia's most-wanted terrorist, police said Sunday, shooting the suspect in the leg as he tried to escape a raid on his hide-out. A car bomb claimed by dissident Republican paramilitaries cast a cloud Monday over the transfer of key powers from London to Northern Ireland. Billy Wilder's death on Wednesday snuffed out the last of cinema's great Austro-German brigade, a generation of filmmakers who brought to Hollywood a style and a set of attitudes we still feel even if we can't quite place the names. Murnau was the poetic one, von Stroheim in the A Bangladeshi with the same nickname as a murder suspect was mistakenly tried and given the death sentence for a crime he did not commit, his lawyers told a court Wednesday. If President Bush or Vice President Dick Cheney were ever to be impeached, their foes could cite this Independence Day as a milestone -- the day that the nation's first ``impeachment headquarters'' opened its doors. Negotiators for Hollywood actors and producers were meeting Saturday in an eleventh-hour effort to reach a new contract and avert what could be a crippling walkout for the TV and movie industry. Human rights group Amnesty International (AI) said Monday that it had sent a second delegation to Israel on a fact-finding mission to investigate violent clashes in Israel and the occupied territories."
+    #forest = get_minforest(database['article'], 256)
+    lsh = minhash_lsh(database['article'], 0.3, 256)
 
-    result = query(q, database, 256, 3, forest)
-    print(result)
+    q = "The dollar tumbled to record lows against the yen and German mark Tuesday as currency markets were roiled by the Mexican economic crisis and investors shunned the greenback as a ""safe haven"" currency. There is no alien world behind the virtual reality gear, just a modestly decorated living room that can be seen without the video goggles. Chinese Internet portal Tencent Holdings said it will acquire a 10.26 percent stake in Digital Sky Technologies Ltd (DST), a Russian Internet investment firm and Facebook shareholder. Clearly Buhner, who spent 14 years with the Mariners, felt the love from a Safeco Field crowd of 40,805 -- and reciprocated. European countries were urged Thursday ""not to wait for another Madrid or Beslan"" and to take urgent, concrete steps to fight global terrorism. Japan has warned leaders of the House of Representatives that serious, long-term damage to Japanese-U.S. relations is likely if the House passes a resolution demanding an official apology from Japan for its wartime policy of forcing women to become sex slaves for Japanese soldiers. AMF Bowling Worldwide, the world's largest operator of bowling centers, filed for bankruptcy protection Tuesday under a plan that would allow it to repay debt and restructure its operations. China has recently set up a special work group to coordinate its efforts in cracking down on the crimes of producing and selling shoddy and fake products."
+
+
+    #result = query(q, database, 256, 3, forest)
+    result_lsh = query_lsh(q, database, lsh, 256)
+    print(result_lsh)
+    #print(result)
     result_matrix = []
-
+"""
     for index, set_1 in enumerate(shingle_sets):
         result_matrix.append([])
 
@@ -70,3 +75,4 @@ if __name__ == "__main__":
             for col in range(row + 1, size):
                 output.write(f",{result_matrix[col][row]}")
             output.write("\n")
+"""
--- src/minhash.py
+++ src/minhash.py
@@ -4,7 +4,7 @@ from jaccard import ShingleSetGenerator, jaccard
 from collections.abc import Generator, Iterable
 from csv import reader
 from re import split
-from datasketch import MinHash, MinHashLSHForest
+from datasketch import MinHash,MinHashLSH, MinHashLSHForest
 import time
 import re
 import numpy as np
@@ -18,6 +18,19 @@ def preprocess(text) -> list[str]:
     return tokens
 
 
+def create_minhash(data, perm):
+    start = time.time()
+    minhash = []
+    for text in data:
+        tokens = preprocess(text)
+        m = MinHash(num_perm=perm)
+        for shingle in tokens:
+            m.update(shingle.encode('utf8'))
+        minhash.append(m)
+    print('It took %s seconds to build minhash.' % (time.time() - start))
+    return minhash
+
+
 def get_minforest(data, perm):
     """
     This function returns the Min LSH Forest of size perm
@@ -26,21 +39,23 @@ def get_minforest(data, perm):
     :return:
     """
     start = time.time()
-
-    minhash = []
-    for text in data:
-        tokens = preprocess(text)
-        m = MinHash(num_perm=perm)
-        for shingle in tokens:
-            m.update(shingle.encode('utf8'))
-        minhash.append(m)
+    minhash = create_minhash(data, perm)
     minforest = MinHashLSHForest(num_perm=perm)
     for i, m in enumerate(minhash):
         minforest.add(i, m)
-
     minforest.index()
     print('It took %s seconds to build forest.' %(time.time()-start))
     return minforest
+
+
+def minhash_lsh(data, treshhold, perm):
+    start = time.time()
+    minhash = create_minhash(data, perm)
+    minLSH = MinHashLSH(threshold=treshhold, num_perm=perm)
+    for i, m in enumerate(minhash):
+        minLSH.insert(str(i), m)
+    print('It took %s seconds to build LSH index.' % (time.time() - start))
+    return minLSH
 
 
 def query(text: str, db, perm, results, minforest):
@@ -53,5 +68,16 @@ def query(text: str, db, perm, results, minforest):
     index = np.array(minforest.query(m, results))
     if len(index)==0:
         return None
+    result = db.iloc[index]['article']
+    return result
+
+
+def query_lsh(text: str, db, lsh, perm):
+    tokens = preprocess(text)
+    m = MinHash(num_perm=perm)
+    for shingle in tokens:
+        m.update(shingle.encode('utf8'))
+    index = lsh.query(m)
+    print("This is the amount of results: {}".format(index))
     result = db.iloc[index]['article']
     return result
